73c80ecf1fd694d4ff0aaeee6f70e96a029cf63c
rahulprasad5080 <rahulprasad5080@gmail.com>
2025-12-16 15:11:54 +0530

    fghgfh

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
-app.options("*", cors());
+app.options(/.*/, cors());
 
 app.get('/', (req, res) => {
     res.send('API is running...');
